9225dbc4551503140221736cf6211fe81fd96606
jose aparicio <axle982@gmail.com>
2022-12-06 18:13:04 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/Start', (req, res) => {
+    console.log(req);
+    res.send('Got a POST request')
+  })
+
+  app.post('/Stop', (req, res) => {
+    console.log(req);
+    res.send('Got a POST request')
+  })
+
